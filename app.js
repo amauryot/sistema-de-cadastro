303c2bb9ca6190e264d561c5220d19d0e5ddd1e7
Amaury Tavares <amauryot@gmail.com>
2022-11-24 02:56:49 -0300

    console log indicando modificacoes do paulo

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando ao banco de dados");
+// main um console log de teste:
+console.log("O paulo fez esse commit sem voce ver na aula!");
+
 
